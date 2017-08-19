31874e4acfa1f589324c40f1b11d80b8d0e4370b
Matheus Vilvert <matheusvilvert@gmail.com>
2017-08-19 16:35:12 -0300

    login com nivel de acesso

--- validate.php
+++ validate.php
@@ -9,19 +9,21 @@ $link = DBConnect();
         $senha = mysqli_real_escape_string($link,$_POST['senha']);
         $senha = md5($senha);
 
-        $result = DBRead('usuario',"WHERE email = '$email' && senha = '$senha' LIMIT 1");
-
-        if (empty($result)){
+        $usuario= DBRead('usuarios',"WHERE email = '$email' && senha = '$senha' LIMIT 1");
+        
+        if (empty($usuario)){
             $_SESSION['loginErro'] = "Usuário ou senha Inválido";
             header("Location: index.php");
         }else{
-            header("Location: administrativo.php");
+            if ($usuario[0]["nivel_acesso"] == '1'){
+                header("Location: administrativo.php");
+            }else{
+                header("Location: vendedor.php");
+            }
         }
     }else{
         $_SESSION['loginErro'] = "Usuário ou senha incorretos";
         header("Location: index.php");
     }
-
-
 
     ?>
